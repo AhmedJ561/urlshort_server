75c084f51a0561fc99ef3e6ffcc63ad9f26b100d
AhmedJ561 <138435247+AhmedJ561@users.noreply.github.com>
2025-06-25 23:47:36 +0500

    fix3

--- index.js
+++ index.js
@@ -5,20 +5,26 @@ const staticRoute = require("./routes/staticRouter");
 const cors = require("cors");
 const app = express();
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-
+// Connect to MongoDB with error handling
 connectToMongoDB(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("MongoDB connection error:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
+  });
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+
+// Use routes
 app.use("/url", urlRoute);
 app.use("/server/home", staticRoute);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Vercel handler
 const serverless = require("serverless-http");
