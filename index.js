cd4dd75eb2ec10bcb62e93de5eb9765aec35569d
AhmedJ561 <138435247+AhmedJ561@users.noreply.github.com>
2025-06-25 23:15:29 +0500

    c3

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const staticRoute = require("../routes/staticRouter");
 const cors = require("cors");
 const app = express();
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 connectToMongoDB(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Error:", err));
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
