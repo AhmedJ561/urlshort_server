5b2b14b055b9267e74bda1eec2c04c3e263546fb
AhmedJ561 <138435247+AhmedJ561@users.noreply.github.com>
2026-05-20 13:25:39 +0500

    new

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
 const cors = require("cors");
+const Url = require("./models/url");
 const app = express();
 
 const corsOptions = {
   origin: [
     "https://ahmedj561.github.io",
+
+    "https://urlshort-server.vercel.app",
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
@@ -33,6 +36,25 @@ app.use("/server/home", staticRoute);
 
 app.get("/", (req, res) => {
   res.status(200).send("URL Shortener API is running");
+});
+
+app.get("/:shortId", async (req, res) => {
+  try {
+    const shortId = req.params.shortId;
+    const entry = await Url.findOneAndUpdate(
+      { shortId },
+      { $push: { visitHistory: { timestamp: Date.now() } } }
+    );
+
+    if (entry) {
+      res.redirect(entry.redirectUrl);
+    } else {
+      res.status(404).send("URL not found");
+    }
+  } catch (error) {
+    console.error("Error occurred while redirecting:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Error handling middleware
